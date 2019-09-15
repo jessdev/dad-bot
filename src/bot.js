42b6feb37545cc6e01799bb0f926284d409a11fa
Jessica DeVriese <jessdev@umich.edu>
2019-09-14 22:17:23 -0400

    sort of working

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ var auth = require('./auth.json');
 var api = require('./twitch-api');
 
 const client = new Discord.Client();
+console.log(api);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,6 +12,18 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!get-streams') {
+    var streams = api.topStreams;
+    var message = "";
+    var length = 5;
+    for(var i = 0; i< length; i++){
+      message = message + " "+ streams[i].name;
+    }
+    msg.reply(message);
   }
 });
 
--- src/twitch-api.js
+++ src/twitch-api.js
@@ -4,19 +4,19 @@ const twitchApi = 'https://api.twitch.tv/helix/';
 let authToken = "fuckall";
 
 var users = {
-    byLogin: function(loginName){
-        return axios.get(twitchApi+'users?llogin='+loginName).catch(error =>{
+    byLogin: function (loginName) {
+        return axios.get(twitchApi + 'users?llogin=' + loginName).catch(error => {
             console.error(error);
         });
     }
 }
 
 var authentication = {
-    getToken: function(){
-        getRequest('https://id.twitch.tv/oauth2/token?client_id='+auth.twitch.ClientId
-        + '&client_secret='+ auth.twitch.ClientSecret
-        + '&grant_type=client_credentials'
-        + '&scope=user:edit'
+    getToken: function () {
+        getRequest('https://id.twitch.tv/oauth2/token?client_id=' + auth.twitch.ClientId
+            + '&client_secret=' + auth.twitch.ClientSecret
+            + '&grant_type=client_credentials'
+            + '&scope=user:edit'
         );
     }
 };
@@ -35,12 +35,43 @@ var authentication = {
 //     console.log(response);
 // });
 
- function getRequest(url){
+var client = axios.create({
+    baseUrl: "",
+    headers: { 'Client-ID': auth.twitch.ClientId }
+});
+
+var topStreams = [];
+
+client.get('https://api.twitch.tv/helix/streams?game_id=33214').then((response) => {
+    topStreams = response.data.data;
+    var list = [];
+    for(var i = 0; i<5; i++){
+        list.push({name: topStreams[i].title, img: topStreams[i].thumbnail_url});
+    }
+    topStreams = list;
+}).catch(error => {
+    console.error(error);
+    console.error("Error occured at url: " + url)
+});
+
+function getRequest(url) {
     return axios({
         method: 'GET',
         url: url,
-    }).catch(error =>{
+    }).catch(error => {
         console.error(error);
-        console.error("Error occured at url: "+ url)
-    })
+        console.error("Error occured at url: " + url)
+    });
 }
+
+function getTop5Streams(){
+    var list = [];
+    for(var i = 0; i<5; i++){
+        list.push({name: topStreams[i].title, img: topStreams[i].thumbnail_url});
+    }
+}
+
+module.exports = {
+    topStreams: topStreams,
+    getTop5Streams: getTop5Streams
+};
