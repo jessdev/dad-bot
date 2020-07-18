7b1dae8dd049fc331dd42b3dd1a7679497e4e38f
Jessica DeVriese <jessdev@umich.edu>
2020-07-18 00:05:13 -0400

    cleaned up

--- src/meme-service.js
+++ src/meme-service.js
@@ -1,19 +1,28 @@
 
-function registerMemes(client){
-    client.on('message', msg => {
-        if(msg.content.indexOf("!familiar") > -1){
-          msg.channel.send("[sad wizard noises]");
+function registerMemes(client) {
+    client.on("message", msg =>{
+        if (msg.content.indexOf("!familiar") > -1) {
+            msg.channel.send("[sad wizard noises]");
         }
-        else if (msg.content.indexOf("!troll") > -1){
-          msg.channel.send("And then, all the trolls died from network connectivity issues.");
+    });
+
+    client.on("message", msg =>{
+        if (msg.content.indexOf("!troll") > -1) {
+            msg.channel.send("And then, all the trolls died from network connectivity issues.");
         }
-        else if(msg.content.indexOf("do it") > -1){
-          msg.channel.send("I didn't ask how big the room is. I said I cast fireball.");
+    });
+
+    client.on("message", msg =>{
+        if (msg.content.indexOf("do it") > -1) {
+            msg.channel.send("I didn't ask how big the room is. I said I cast fireball.");
         }
-        else if(msg.content.toLowerCase().includes("!torch")){
-          msg.channel.send("THE TORCH. GIVE EM THE TORCH");
+    });
+
+    client.on("message", msg =>{
+        if (msg.content.toLowerCase().includes("!torch")) {
+            msg.channel.send("THE TORCH. GIVE EM THE TORCH");
         }
-      });
+    });
 }
 
 module.exports = {
